f8243760b65b7ec8cd308b2d553d890062da8529
lflaw1554-debug <lflaw1554@gmail.com>
2026-05-07 09:25:03 +0700

    data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+const islamicQueries = [
+  "doa harian islami",
+  "asmaul husna lengkap",
+  "jadwal sholat bogor",
+  "keutamaan tahajud",
+  "kisah nabi muhammad",
+  "cara zakat mal"
+];
+
+const msnLinks = [
+  "https://www.msn.com",
+  "https://www.msn.com/en-gb/news",
+  "https://www.msn.com/en-gb/money",
+  "https://www.msn.com/en-gb/health"
+];
