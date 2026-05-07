4291e3d53d593bdb32ce876638679c92e1cd4de0
lflaw1554-debug <lflaw1554@gmail.com>
2026-05-07 16:17:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,17 +18,28 @@ function writeLog(msg, color = "#444") {
 // DATA
 const msnLinks = [
   "https://www.msn.com",
-  "https://www.msn.com/en-gb/news",
-  "https://www.msn.com/en-gb/money"
-];
-
+  "https://www.msn.com/en-gb/travel/news/holidaymakers-told-to-arrive-four-hours-early-or-miss-flights-in-ees-chaos/ss-AA22lkS6?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/news/uknews/starmer-under-threat-over-trojan-horse-plot-to-smuggle-rival-back-to-replace-him/ar-AA22wWGb?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/entertainment/tv/badenoch-starmer-s-finished-and-polanski-s-a-joke-we-re-the-only-serious-choice/ar-AA22xk97?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/entertainment/news/king-charles-allegedly-slams-top-dog-authority-prince-william-told-to-know-his-place/ar-AA22wXBc?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/travel/news/coventry-airport-to-close-after-90-years-with-all-flights-scrapped-within-weeks/ar-AA22wiSz?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/money/other/aldi-and-lidl-rule-could-be-forced-to-change-as-uk-supermarkets-demand-crackdown/ar-AA22wfrY?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/news/uknews/starmer-s-last-ditch-plea-as-labour-braces-for-local-election-disaster/ar-AA22yUxG?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/travel/news/uk-staycation-demand-rises-as-airlines-told-they-can-cancel-flights/ss-AA22qn5L?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/lifestyle/family-relationships/report-queen-camilla-s-ex-husband-now-right-hand-man-to-princess-anne/ss-AA22i4Ea?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/foodanddrink/other/binman-s-rule-to-anyone-recycling-tins-and-jars-with-labels-left-on/ar-AA1X2DRu?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/money/other/britain-sleepwalking-towards-jobless-generation-warn-retailers/ar-AA22AqRQ?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/news/uknews/keir-starmer-to-use-eu-reset-to-stave-off-leadership-challenge/ar-AA22zFsq?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/money/technology/all-gmail-users-urged-to-check-one-setting-now-as-important-warning-issued/ar-AA22ueZv?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  "https://www.msn.com/en-gb/news/world/sky-news-halted-for-ultimate-humiliation-breaking-news-alert-for-donald-trump/ar-AA22vBj6?cvid=69fc548eb56e4d098c433a606fdef345&ocid=sappandhp",
+  ];
 const islamicQueries = [
-  "islam",
-  "quran",
-  "masjid",
-  "shalat",
-  "ramadhan",
-  "kajian islam"
+  "https://www.bing.com/search?pglt=2339&q=jokg&cvid=7da368f7858b411e964fc5eafc764e0f&gs_lcrp=EgRlZGdlKgYIABBFGDkyBggAEEUYOTIGCAEQABhAMgYIAhAuGEAyBggDEAAYQDIGCAQQABhAMgYIBRAAGEAyBggGEAAYQDIGCAcQABhAMgYICBAAGEDSAQgxNzQwajBqN6gCALACAA&FORM=ANNTA1&PC=ASTS",
+  "https://www.bing.com/search?q=liga1&qs=n&form=QBRE&sp=-1&ghc=1&lq=0&pq=liga1&sc=12-5&sk=&cvid=59C766EE579D4ED289989F08D38D26AD",
+  "https://www.bing.com/search?q=laliga&cvid=987f1079d0bf4c249fe969fdc08b384c&gs_lcrp=EgRlZGdlKgYIABBFGDkyBggAEEUYOTIGCAEQABhAMgYIAhAAGEAyBggDEAAYQDIGCAQQABhAMgYIBRAAGEAyBggGEAAYQDIGCAcQABhAMgYICBAAGEDSAQgzMDkwajBqOagCALACAA&FORM=ANAB01&PC=ASTS",
+  "https://www.bing.com/search?q=liga+inggris&cvid=edab4d22395348f282ca6fad2dfa27be&gs_lcrp=EgRlZGdlKgYIABBFGDkyBggAEEUYOTIGCAEQIxgnMgYIAhAAGEAyBggDEAAYQDIGCAQQABhAMgYIBRAAGEAyBggGEAAYQDIGCAcQABhAMgYICBAAGEDSAQgzOTEzajBqOagCALACAA&FORM=ANAB01&PC=ASTS",
+  "https://www.bing.com/search?q=cuaca+hari+ini&cvid=af262c68d22a493abb1ea088831580b8&gs_lcrp=EgRlZGdlKgYIABBFGDkyBggAEEUYOTIGCAEQABhAMgYIAhAAGEAyBggDEAAYQDIGCAQQABhAMgYIBRAAGEAyBggGEAAYQDIGCAcQABhAMgYICBAAGEDSAQgzNTYzajBqOagCALACAA&FORM=ANAB01&PC=ASTS",
+  "https://www.bing.com/search?q=jokes%20hari%20ini&qs=n&form=QBRE&sp=-1&lq=0&pq=jokeshari%20ini&sc=10-13&sk=&cvid=ADDC268017EF4413AAF75F208A699472"
 ];
 
 async function runProPlayer() {
